2e2557307fa07623d15e598f68234ca867676f9c
petidesoftwares <petideenterprise@gmail.com>
2020-11-20 15:39:57 +0100

    Update arms-index.js

--- custom-jscript/arms-index.js
+++ custom-jscript/arms-index.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 
 function hideMenu(){
     $("#menu-list").hide(500);
+    $(".menu-frame").show();
     $("#dashboard-item-pane-alter").hide();
     $("#dashboard-item-pane").show();
 }
@@ -23,5 +24,54 @@ function showDashboard(){
         $("#display-pane").html(data);
         $("#dashboard-item-pane-alter").hide();
         hideMenu();
-    })
+    });
+}
+
+function uploadCourseView(){
+    $.post("views/upload-courses.html",function(data){
+        $("#display-pane").html(data);
+        hideMenu();
+    });
+}
+
+function registerStudents(){
+    $.post("views/register-students.html",function(data){
+        $("#display-pane").html(data);
+        hideMenu();
+    });
+}
+
+function viewStudents(){
+    $.post("views/view-students.html",function(data){
+        $("#display-pane").html(data);
+        hideMenu();
+    });
+}
+
+function viewAttendance(){
+    $.post("views/view-attendance.html",function(data){
+        $("#display-pane").html(data);
+        hideMenu();
+    });
+}
+
+function uploadResult(){
+    $.post("views/result-upload.html",function(data){
+        $("#display-pane").html(data);
+        hideMenu();
+    });
+}
+
+function viewGeneralResult(){
+    $.post("views/general-result.html",function(data){
+        $("#display-pane").html(data);
+        hideMenu();
+    });
+}
+
+function viewIndividualResult(){
+    $.post("views/individual-result.html",function(data){
+        $("#display-pane").html(data);
+        hideMenu();
+    });
 }
