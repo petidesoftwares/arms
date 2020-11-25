69258c3dcfad46ec6062fab8ed2c6034e2bb85b2
petidesoftwares <petideenterprise@gmail.com>
2020-11-25 15:12:54 +0100

    Update db_conn.php

--- backend/db_conn.php
+++ backend/db_conn.php
@@ -1,27 +1,41 @@
 <?php
 class DB_CONNECTION{
-    var $HOST='localhost';
-    var $USER_NAME='root';
-    var $PASSWORD = '';
-    var $DB='myschool_arms';
+    
+    var $cleardb_url;
+    var $cleardb_server;
+    var $cleardb_username;
+    var $cleardb_password;
+
+    public function __construct(){
+        //Get Heroku ClearDB connection information
+        $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $cleardb_server   = $cleardb_url["host"];
+        $cleardb_username = $cleardb_url["user"];
+        $cleardb_password = $cleardb_url["pass"];
+        $cleardb_db       = substr($cleardb_url["path"],1);
+
+        $active_group = 'default';
+        $query_builder = TRUE;
+    }
+
     
     function getHost(){
-        return $this->HOST;
+        return $this->$cleardb_server;
     }
     function getUsername(){
-        return $this->USER_NAME;
+        return $this->$cleardb_username;
     }
     function getPassword(){
-        return $this->PASSWORD;
+        return $this->$cleardb_password;
     }
     function getDB(){
-        return $this->DB;
+        return $this->$cleardb_db;
     }
 
     function createConnection(){
-        $conn = mysqli_connect($this->HOST,$this->USER_NAME,$this->PASSWORD) or die('No Connection Established');
+        $conn = mysqli_connect($this->$cleardb_server,$this->$cleardb_username,$this->$cleardb_password) or die('No Connection Established');
         if($conn){
-            if(mysqli_select_db($conn, $this->DB)==true){
+            if(mysqli_select_db($conn, $this->$cleardb_db)==true){
                 return $conn;
             }
         }
