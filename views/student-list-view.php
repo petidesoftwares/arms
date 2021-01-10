9cb93efa9c9f0b7aa1b1b756ebac4e69a24007c5
petidesoftwares <petideenterprise@gmail.com>
2021-01-10 07:50:58 +0100

    Update student-list-view.php

--- views/student-list-view.php
+++ views/student-list-view.php
@@ -24,7 +24,7 @@
                 $s_n = 0;
                 
                 foreach($students as $student){
-                    if(array_key_exists('othername', $student)){
+                    if(array_key_exists("othername", $student)){
                         $s_n++;
                         if($s_n%2==0){
                             echo '<tr id = "grey-row"><td>'.$s_n.'</td><td id = "getStudent_id_'.$s_n.'" onclick = "getStudentId('.$s_n.')">'.$student['matno'].'</td><td onclick = "getStudentId('.$s_n.')">'.$student['surname'].', '.$student['firstname'].' '.$student['othername'].'</td onclick = "getStudentId('.$s_n.')"></tr>';
@@ -32,7 +32,6 @@
                             echo '<tr><td>'.$s_n.'</td><td id = "getStudent_id_'.$s_n.'" onclick = "getStudentId('.$s_n.')">'.$student['matno'].'</td><td onclick = "getStudentId('.$s_n.')">'.$student['surname'].', '.$student['firstname'].' '.$student['othername'].'</td onclick = "getStudentId('.$s_n.')"></tr>';
                         }   
                     }else{
-                        echo "hhhhh";
                         $s_n++;
                         if($s_n%2==0){
                             echo '<tr id = "grey-row"><td>'.$s_n.'</td><td id = "getStudent_id_'.$s_n.'" onclick = "getStudentId('.$s_n.')">'.$student['matno'].'</td><td onclick = "getStudentId('.$s_n.')">'.$student['surname'].', '.$student['firstname'].'</td onclick = "getStudentId('.$s_n.')"></tr>';
